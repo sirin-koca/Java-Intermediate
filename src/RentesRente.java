dec05cf5d63617b23fdc63ffa7b599eb38becaab
Sirin Koca <s182211@oslomet.no>
2022-09-29 22:13:24 +0200

    Java Basic concepts - RentesRente.java

---
+++ src/RentesRente.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.text.NumberFormat;
+
+// Beregning av rentesrente.
+import java.text.NumberFormat; // numerisk formatering
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+public class RentesRente {
+    public static void main(String args[]) {
+        double beløp;   // totalbeløp ved slutten av hvert år
+        double grunnbeløp = 1000.0; //grunnbeløpet forrentet
+        double rentefot = 0.05; // rentefot pro anno
+
+        // oppretter formateringsobjekt for kronebeløp
+        NumberFormat kroneformat = NumberFormat.getCurrencyInstance();
+
+        // oppretter JTextArea for visning av utskrift
+        JTextArea tekstområde = new JTextArea();
+
+        // lager overskrift i tekstområdet
+        tekstområde.setText("år\ttotalbeløp\n");
+
+        // beregner totalbeløp for hvert år i 10 år
+        for (int år = 1; år <= 10; år++) {
+            // beregner nytt beløp for gjeldende år
+            beløp = grunnbeløp * Math.pow(1.0 + rentefot, år);
+
+            // tilføyer en tekstlinje i tekstområdet
+            tekstområde.append(
+                    år + "\t" + kroneformat.format(beløp) + "\n");
+        } // end for
+
+        // viser resultater
+        JOptionPane.showMessageDialog(null, tekstområde,
+                "Rentesrente", JOptionPane.INFORMATION_MESSAGE);
+
+    } // end main
+} // end klasse Renteberegning
+
