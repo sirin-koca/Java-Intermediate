e1b3b9c839dafc81be48ad4cdfd499d2c6fbf6a4
Sirin Koca <s182211@oslomet.no>
2022-10-05 02:14:06 +0200

    Fibonacci numbers

---
+++ src/Fibonacci.java
@@ -0,0 +1,41 @@
+public class Fibonacci {
+    /**
+     * O(n)
+     * @param n
+     * @return
+     */
+    public static int fibonacci(int n) {
+        if(n<0){
+            return -1;
+        }
+        if (n <= 1) {
+            return n;
+        }
+        int pre = 1;
+        int prepre = 1;
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = pre + prepre;
+            prepre = pre;
+            pre = result;
+        }
+        return result;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(Fibonacci.fibonacci(0));
+        System.out.println(Fibonacci.fibonacci(1));
+        System.out.println(Fibonacci.fibonacci(2));
+        System.out.println(Fibonacci.fibonacci(3));
+        System.out.println(Fibonacci.fibonacci(4));
+        System.out.println(Fibonacci.fibonacci(5));
+        System.out.println(Fibonacci.fibonacci(6));
+        System.out.println(Fibonacci.fibonacci(7));
+
+    }
+
+}
