8f4cb05ba6518e25a78e76c3b204b745b97451a8
sirin-koca <s182211@oslomet.no>
2022-10-23 15:05:20 +0200

    understandRecursion() implemented

---
+++ src/Recursion.java
@@ -0,0 +1,27 @@
+import static javax.swing.JOptionPane.*;
+
+class Recursive{
+    public boolean understandRecursion(String doIUnderstandRecursion) {
+
+    String recursionAnswer = showInputDialog(null, "Do you understand recursion?\nPlease type: Y/N");
+        if(recursionAnswer.equals("Y")) { // base case
+            showMessageDialog(null, "Good job!");
+            return true;
+        }
+        if(recursionAnswer.equals("N")) { // base case
+            understandRecursion(doIUnderstandRecursion); // recursive call
+        }
+        else {
+            showMessageDialog(null, "Something went wrong, try again!");
+        }
+        return false;
+    }
+}
+
+public class Recursion {
+    public static void main(String[] args) {
+        Recursive call = new Recursive();
+        call.understandRecursion("");
+    }
+}
+
