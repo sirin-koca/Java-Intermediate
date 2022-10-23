d8f99b928092a5378842a7911ff8223bf0a99725
sirin-koca <s182211@oslomet.no>
2022-10-23 18:59:07 +0200

    WhileLoop with try-catch

---
+++ src/Gjennomsnitt.java
@@ -0,0 +1,16 @@
+
+public class Gjennomsnitt {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int teller = 0;
+        while (sum < 100){
+            sum += 2;
+            teller++;
+            System.out.println(sum);
+        }
+        System.out.println("The sum is: " + sum);
+        System.out.println("The counter: " + teller);
+        System.out.println("The avarege is: " + sum/teller);
+    }
+}
