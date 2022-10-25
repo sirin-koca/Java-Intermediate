027707d9f564c6c318583b77c4b1cfcd6526201f
sirin-koca <s182211@oslomet.no>
2022-10-25 10:56:57 +0200

    Examples on Java-methods and objects

--- src/ObjectsAndClasses/BookObjects.java
+++ src/ObjectsAndClasses/BookObjects.java
@@ -1,14 +1,15 @@
+package ObjectsAndClasses;
 
 import static javax.swing.JOptionPane.*;
 
 class MyBooks {
     private String tittel;
-    private String forfatter;
+    private String author;
     private double pris;
 
-    public MyBooks(String tittel, String forfatter, double pris) {
+    public MyBooks(String tittel, String author, double pris) {
         this.tittel = tittel;
-        this.forfatter = forfatter;
+        this.author = author;
         if (pris > 0) {  // NB: Må være med her også!
             this.pris = pris;
         }
@@ -22,12 +23,12 @@ class MyBooks {
         this.tittel = tittel;
     }
 
-    public String getForfatter() {
-        return forfatter;
+    public String getAuthor() {
+        return author;
     }
 
-    public void setForfatter(String forfatter) {
-        this.forfatter = forfatter;
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public double getPris() {
@@ -40,7 +41,7 @@ class MyBooks {
         }
     }
 
-    public double regnUtRabatt(double prosent) {
+    public double discount(double prosent) {
         double rabatt = pris * (prosent / 100);
         return rabatt;
     }
@@ -48,7 +49,7 @@ class MyBooks {
     public String formaterUt() {
         String ut = "Her er bokinformasjonen : \n" +
                 "Tittel :  " + tittel + "\n" +
-                "Forfatter :" + forfatter + "\n" +
+                "Forfatter :" + author + "\n" +
                 "Pris :" + pris + "\n";
         return ut;
     }
@@ -59,20 +60,24 @@ public class BookObjects {
     public static void main(String[] args) {
         // Definerer paramatre som skal inni Bok-objektet.
         String tittel = showInputDialog("Skriv inn tittel");
-        String forfatter = showInputDialog("Skriv inn forfatter");
+        String author = showInputDialog("Skriv inn forfatter");
         String innPris = showInputDialog("Skriv inn prisen");
-        double pris = Double.parseDouble(innPris); // Input alltid String så vi må bruke Casting for å konvertere til Double
+        double price = Double.parseDouble(innPris); // Input alltid String så vi må bruke Casting for å konvertere til Double
 
-        MyBooks book1 = new MyBooks(tittel, forfatter, pris);
-
+        MyBooks book1 = new MyBooks(tittel, author, price);
         String person = showInputDialog("Ansatt eller student (A eller S)?");
         if (person.equals("A")) {
-            double rabatt = book1.regnUtRabatt(15);
+            double rabatt = book1.discount(15);
             double nyPris = book1.getPris() - rabatt;
             book1.setPris(nyPris);
         }
-        String ut = book1.formaterUt();
-        showMessageDialog(null, ut);
+        String ut1 = book1.formaterUt();
+        showMessageDialog(null, ut1);
+
+        // Make a book object and print out:
+        MyBooks book2 = new MyBooks(tittel, author, price);
+        String ut2 = book2.formaterUt();
+        showMessageDialog(null, ut2);
 
     }
 }
