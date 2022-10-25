07dd08059afebc6337361a70e7a86a3e13cf11d3
sirin-koca <s182211@oslomet.no>
2022-10-25 09:49:30 +0200

    Examples on ArraysToString.java

--- src/PrimeNumberChecker.java
+++ src/PrimeNumberChecker.java
@@ -1,19 +1,24 @@
 public class PrimeNumberChecker {
     static void checkPrime(int n) {
-        int i, m = 0, redFlag = 0;
+
+        int i, m, redFlag = 0;
+
         m = n / 2;
+
         if (n == 0 || n == 1) {
-            System.out.println(n + " is not prime number");
+            System.out.println(n + " is not a prime number");
+
         } else {
             for (i = 2; i <= m; i++) {
+
                 if (n % i == 0) {
-                    System.out.println(n + " is not prime number");
+                    System.out.println(n + " is not a prime number");
                     redFlag = 1;
                     break;
                 }
             }
             if (redFlag == 0) {
-                System.out.println(n + " is prime number");
+                System.out.println(n + " is a prime number");
             }
         }//end of else
     }
@@ -23,5 +28,7 @@ public class PrimeNumberChecker {
         checkPrime(3);
         checkPrime(17);
         checkPrime(20);
+        checkPrime(24);
+        checkPrime(41);
     }
 }
