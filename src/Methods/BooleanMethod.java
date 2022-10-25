34c43bedfab290bf9af1ef3a1be2e96cefa866c4
sirin-koca <s182211@oslomet.no>
2022-10-26 00:18:21 +0200

    BooleanMethod.java

---
+++ src/Methods/BooleanMethod.java
@@ -0,0 +1,33 @@
+package Methods;
+
+public class BooleanMethod {
+    public static void main(String[] args) {
+
+        boolean passedDATS2300 = true;
+        boolean passedDAFE2200 = true;
+        boolean passedDAPE1300 = true;
+
+        boolean passedX = false;
+
+        int counter = 0;
+
+        if (passedDATS2300) {
+            System.out.println("You have passed DATS2300: Algorithms and Datastructures!");
+            counter++;
+        }
+        if (passedDAFE2200) {
+            System.out.println("You have passed DAFE2200: Software Engineering!");
+            counter++;
+        }
+        if (passedDAPE1300) {
+            System.out.println("You have passed DAPE1300: Discrete Mathematics!");
+            counter++;
+        }
+        if(passedX){
+            System.out.println("You dont take this class.");
+        }
+        if (counter == 3){
+            System.out.println("Congrats!");
+        }
+    }
+}
