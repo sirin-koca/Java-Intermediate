636d135620888e67213c6a0a42b9107d10dd2acc
sirin <s182211@outlook.com>
2023-01-27 17:27:14 +0100

    new examples of java

---
+++ src/AsciiArt.java
@@ -0,0 +1,7 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println(" /\\_/\\ ");
+        System.out.println("( o.o ) ");
+        System.out.println(" > ^ <  ");
+    }
+}
