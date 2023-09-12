d762641e4286cbfbaa140ced9a8f04b8b2496e8f
Sirin Koca <s182211@oslomet.no>
2023-09-13 01:53:34 +0200

    Update Abstraction2.java

--- src/Abstraction2.java
+++ src/Abstraction2.java
@@ -1,6 +1,5 @@
 class Main {
    public static void main(String[] args) {
-       //do not touch this code
        Monopoly monopoly = new Monopoly();
        Chess chess = new Chess();
        Battleships battleships = new Battleships();
