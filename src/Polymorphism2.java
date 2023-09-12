1f9ab92dd858f6b6d098d3694da2409a7d120977
Sirin Koca <s182211@oslomet.no>
2023-09-13 00:44:07 +0200

    Create Polymorphism2.java

---
+++ src/Polymorphism2.java
@@ -0,0 +1,38 @@
+// Polymorphism is in short: one method, with different implementations.
+
+class Main {
+   public static void main(String[] args) {       
+       Vehicle vehicle = new Vehicle();
+       Vehicle electric = new ElVehicle();
+       Vehicle hybrid = new HybridVehicle();
+       
+       //calls
+       vehicle.start();
+       vehicle.resource();
+       electric.start();
+       electric.resource();
+       hybrid.start();
+       hybrid.resource();
+   }
+}
+
+class Vehicle{
+    public void start(){
+        System.out.println("Starting");
+    }
+    public void resource(){
+        System.out.println("I use petrol");
+    }
+}
+
+class ElVehicle extends Vehicle{
+        public void resource(){
+        System.out.println("I use electricity");
+    }
+}
+
+class HybridVehicle extends Vehicle{
+        public void resource(){
+        System.out.println("I use both petrol and electricity");
+    }
+}
