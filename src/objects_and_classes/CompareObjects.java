b23d38a9e6184cb2bb4f801daf5b2b6e0ea0d916
Sirin Koca <s182211@oslomet.no>
2023-09-13 14:53:20 +0200

    Update CompareObjects.java

--- src/objects_and_classes/CompareObjects.java
+++ src/objects_and_classes/CompareObjects.java
@@ -12,3 +12,19 @@ class MyClass {
         System.out.println("Is a1 = a2? " + "Answer: " + (a1 == a2));
     }
 }
+
+
+/*
+public static void main(String[ ] args) {
+  Animal a1 = new Animal("Robby");
+  Animal a2 = new Animal("Robby");
+  System.out.println(a1.equals(a2));
+}
+
+OBS! 
+The automatically generated hashCode() method is used to determine where to store the object internally. 
+Whenever you implement equals, you MUST also implement hashCode: 
+
+Just right click in your class, go to Source->Generate hashCode() and equals()...  
+
+*/
