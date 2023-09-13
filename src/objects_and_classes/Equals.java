c710d62d0a84ec13f0b1837b4894cea08d12b788
Sirin Koca <s182211@oslomet.no>
2023-09-13 14:54:45 +0200

    Update Equals.java

--- src/objects_and_classes/Equals.java
+++ src/objects_and_classes/Equals.java
@@ -1,8 +1,22 @@
 /* equals()
 Each object has a predefined equals() method that is used for semantical equality testing.
 But, to make it work for our classes, we need to override it and check the conditions we need.
+
 There is a simple and fast way of generating the equals() method, other than writing it manually.
-Just right click in your class, go to Source->Generate hashCode() and equals()... 
+Just right-click in your class, go to Source->Generate hashCode() and equals()... 
+
+OBS! 
+The automatically generated hashCode() method is used to determine where to store the object internally. 
+Whenever you implement equals, you MUST also implement hashCode: 
+
+Just right-click in your class, go to Source->Generate hashCode() and equals()...  
+
+  public static void main(String[ ] args) {
+    Animal a1 = new Animal("Robby");
+    Animal a2 = new Animal("Robby");
+    System.out.println(a1.equals(a2));
+  }
+
 */
 
 class Animal {
