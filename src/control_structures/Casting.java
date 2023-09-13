f9f25df8041be5d2322350b7717f8eb1f1df1453
Sirin Koca <s182211@oslomet.no>
2023-09-13 14:07:59 +0200

    Create Casting.java

---
+++ src/control_structures/Casting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       char a = read.next().charAt(0);
+       
+       int b = (int)a;
+       System.out.println(b);
+       
+    }   
+}
