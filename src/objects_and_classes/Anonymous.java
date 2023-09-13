fbff20bb822607a60e3c70572644b5487f256782
Sirin Koca <s182211@oslomet.no>
2023-09-13 14:29:27 +0200

    Update Anonymous.java

--- src/objects_and_classes/Anonymous.java
+++ src/objects_and_classes/Anonymous.java
@@ -10,10 +10,18 @@ class Machine {
 // When creating the Machine object, we can change the start method on the fly.
 
 public static void main(String[ ] args) {
-  Machine m = new Machine() {
+  Machine m1 = new Machine() {
     @Override public void start() {
       System.out.println("Wooooo");
     }
   };
-  m.start();
+
+  m1.start(); // starter machine1
+  
+  Machine m2 = new Machine();
+  m2.start(); // starter machine2
 }
+
+/* The modification is applicable only to the current object, and not the class itself. So if we create another object of that class, 
+the start method's implementation will be the one defined in the class. */
+
