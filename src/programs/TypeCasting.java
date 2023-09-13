f0776103035c772e7a5cb7d0debbae4fd2ff43cf
Sirin Koca <s182211@oslomet.no>
2023-09-13 14:13:42 +0200

    Create TypeCasting.java

---
+++ src/programs/TypeCasting.java
@@ -0,0 +1,17 @@
+
+public static void main(String[ ] args) { 
+
+  double x = 1.5;
+
+  double y = 2.65;
+
+  sum((int)x, (int)y);
+
+}
+
+static void sum(int x, int y) {
+
+   System.out.println(x + y);
+
+}
+
