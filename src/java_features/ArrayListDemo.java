3cd95913a6137573377bc4def0b07922c422ab80
sirin <s182211@outlook.com>
2023-09-14 18:18:11 +0200

    refactoring packages

--- src/java_features/ArrayListDemo.java
+++ src/java_features/ArrayListDemo.java
@@ -6,14 +6,19 @@ ArrayLists are created with an initial size, but when this size is exceeded, the
 When objects are removed, the ArrayList may shrink in size. ArrayList class is in the java.util package,
 so it's necessary to import it before using it.
 
-
 ArrayLists store objects. Thus, the type specified must be a class type. You cannot pass, for example, int as
 the objects' type. Instead, use the special class types that correspond to the desired value type, such as Integer
 for int, Double for double, and so on.
 
+We create an ArrayList as you would any object.
 Ex:
-ArrayList<String> colors = new ArrayList<String>(10);
-//The code above defines an ArrayList of Strings with 10 as its initial size.
+    import java.util.ArrayList;
+    //... some code here...
+    ArrayList colors = new ArrayList();
+
+Ex:
+    ArrayList<String> colors = new ArrayList<String>(10);
+    //The code above defines an ArrayList of Strings with 10 as its initial size.
  */
 
 public class ArrayListDemo {
