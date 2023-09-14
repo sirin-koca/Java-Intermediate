ba81c2f621c3081e9786f264a209d814a3de7109
sirin <s182211@outlook.com>
2023-09-14 18:20:20 +0200

    refactoring packages

--- src/java_features/ArrayListDemo.java
+++ src/java_features/ArrayListDemo.java
@@ -10,15 +10,28 @@ ArrayLists store objects. Thus, the type specified must be a class type. You can
 the objects' type. Instead, use the special class types that correspond to the desired value type, such as Integer
 for int, Double for double, and so on.
 
-We create an ArrayList as you would any object.
+We create an ArrayList as you would any object. The add() method adds new objects to the ArrayList.
+Conversely, the remove() method removes objects from the ArrayList.
 Ex:
     import java.util.ArrayList;
     //... some code here...
-    ArrayList colors = new ArrayList();
+    ArrayList<String> colors = new ArrayList<String>();
+    colors.add("Red");
+    colors.add("Blue");
+    colors.add("Green");
+    colors.add("Orange");
+    colors.remove("Green");
+    System.out.println(colors);
 
 Ex:
     ArrayList<String> colors = new ArrayList<String>(10);
     //The code above defines an ArrayList of Strings with 10 as its initial size.
+
+Other useful methods include the following:
+- contains(): Returns true if the list contains the specified element
+- get(int index): Returns the element at the specified position in the list
+- size(): Returns the number of elements in the list
+- clear(): Removes all of the elements from the list
  */
 
 public class ArrayListDemo {
